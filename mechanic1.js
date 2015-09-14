82eaffbd7916bcb4231a2b7e0c86245d591def65
Fisab <vladsudakov5@gmai.com>
2015-09-14 15:18:53 +0400

    Create mechanic1.js

---
+++ mechanic1.js
@@ -0,0 +1,29 @@
+var global = require('global');
+var creeps = require('creeps');
+
+function repairRoad(creep){
+	if(creep.carry.energy == 0){
+		creeps.takeEnergy(creep);
+	}
+	else{
+		var needRepairRoad = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+    		filter: function(object) {
+    			if(object.structureType == 'road'){
+    				return object.hits < object.hitsMax;
+    			}
+    		}
+		}
+		if(needRepairRoad){
+			if(creep.pos.isNearTo(needRepairRoad)){
+				creep.repair(needRepairRoad);
+			}
+			else{
+				creep.moveTo(needRepairRoad);
+			}
+		}
+	}
+}
+
+module.exports = {
+	repairRoad: repairRoad
+}
