079ea80c63738fbe98af60d391320b952da19b57
Fisab <vladsudakov5@gmai.com>
2015-09-14 21:36:28 +0400

    Update global.js

--- global.js
+++ global.js
@@ -1,16 +1,25 @@
 var creeps = require('creeps');
 
-var allCreeps = room.find(FIND_MY_CREEPS);
 var suicideCreepTick = 200;
+
+function getCreepsInRoom(room){
+	var creeps = room.find(FIND_MY_CREEPS);
+	return creeps;
+}
 
 function getSpawn(creep){
 	var closestSpawn = creep.pos.findClosestByPath(FIND_MY_SPAWNS);
 	return closestSpawn;
 }
 
-function room(creep){
+function getRoomCreep(creep){
 	var room = creep.pos.roomName;
-	return Game.rooms.room;		
+	return room;		
+}
+
+function getRoomSpawn(spawn){
+	var room = Game.spawns.spawn.pos.roomName;
+	return room;
 }
 
 var roomOptions {
@@ -21,8 +30,9 @@ var roomOptions {
 
 module.exports = {
 	roomOptions: roomOptions,
-	allCreeps: allCreeps,
+	getCreepsInRoom: getCreepsInRoom,
 	room: room,
 	suicideCreepTick: suicideCreepTick,
-	getSpawn: getSpawn
+	getSpawn: getSpawn,
+	getRoomSpawn: getRoomSpawn
 }
