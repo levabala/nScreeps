2ade0f6c6ca82d0be4e76d97ef026a3af90c0c68
Fisab <vladsudakov5@gmai.com>
2015-09-14 15:18:16 +0400

    Create harvester.js

---
+++ harvester.js
@@ -0,0 +1,40 @@
+var global = require('global');
+
+function harvest(creep, energy){
+	var energy = room.find(FIND_SOURCES);
+	if(creep.pos.isNearTo(energy[harvesterCount%2])){
+		creep.harvest(energy[harvesterCount%2])
+	}
+	else{
+		//when spawn add creep.memory.energy = 0 or 1 =)
+		creep.moveTo(energy[creep.memory.energy]);
+	}
+}
+
+function transferToLink(creep){
+	var linkRange = creep.pos.findInRange(FIND_MY_STRUCTURES, 2);
+	if(linkRange.length > 0){
+        if(creep.carryCapacity == creep.carry.energy){
+            var closestLink = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+				filter: function(object) {
+            	    if(object.structureType !== STRUCTURE_LINK ) {return false;}
+            		return true;
+            	}
+        	});           
+        	if(creep.pos.isNearTo(closestLink)){
+        		creep.transferEnergy(closestLink);
+        	}
+        	else{
+        		creep.moveTo(closestLink);
+        	}
+        }
+    }
+    else{
+        creep.dropEnergy(creep.carry.energy);
+    }
+}
+
+module.exports = {
+	harvest: harvest,
+	transferToLink: transferToLink
+}
