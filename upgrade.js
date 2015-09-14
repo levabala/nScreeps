2627f183ee74c565a0b5be84a1feeafbfbdf22cc
Fisab <vladsudakov5@gmai.com>
2015-09-14 15:19:32 +0400

    Create upgrade.js

---
+++ upgrade.js
@@ -0,0 +1,21 @@
+var global = require('global');
+var creeps = require('creeps');
+
+function upgrade(creep){
+	var room = global.room(creep);
+	if(creep.carry.energy == 0){
+		creeps.takeEnergy(creep);
+	}
+	else{
+		if(creep.pos.isNearTo(creep.room.controller)){
+        	creep.upgradeController(creep.room.controller);
+        }
+        else{
+        	creep.moveTo(creep.room.controller);
+        }
+	}
+}
+
+module.exports = {
+	upgrade: upgrade
+}
