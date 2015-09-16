ad25aa1be09db51d1dfdd94a62ccd9f7eb4c9f81
Fisab <vladsudakov5@gmai.com>
2015-09-16 19:18:09 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var global = require('global');
+var globals = require('globals');
 var creeps = require('creeps');
 var spawn = require('spawn');
 var harvester = require('harvester');
@@ -8,42 +8,25 @@ var mechanic = require('mechanic');
 var mechanic1 = require('mechanic1');
 var transport = require('transport');
 var upgrade = require('upgrade');
+var scout = require('scout');
 
-Memory.rooms.E4N9.needAllCreepsInRoom = 9;
-Memory.rooms.E4N9.harvesterCount = 0;
-Memory.rooms.E4N9.builderCount = 0;
-Memory.rooms.E4N9.guardCount = 0;
-Memory.rooms.E4N9.scoutCount = 0;
-Memory.rooms.E4N9.upgraderCount = 0;
-Memory.rooms.E4N9.mechanicCount = 0;
-Memory.rooms.E4N9.mechanic1Count = 0;
-Memory.rooms.E4N9.transportCount = 0;
-var target;
-spawn.CountCreeps(Game.spawns.Spawn1);
+var room1 = Game.spawns.Spawn1.room;
+var spawn1 = Game.spawns.Spawn1;
 
-Memory.rooms.E4N9.harvesterNeed = 2 - Memory.rooms.E4N9.harvesterCount;
-Memory.rooms.E4N9.builderNeed = 1 - Memory.rooms.E4N9.builderCount ;
-Memory.rooms.E4N9.guardNeed = 1 - Memory.rooms.E4N9.guardCount;
-Memory.rooms.E4N9.scoutNeed = 1 - Memory.rooms.E4N9.scoutCount;
-Memory.rooms.E4N9.upgraderNeed = 1 - Memory.rooms.E4N9.upgraderCount;
-Memory.rooms.E4N9.mechanicNeed = 1 - Memory.rooms.E4N9.mechanicCount;
-Memory.rooms.E4N9.mechanic1Need = 1 - Memory.rooms.E4N9.mechanic1Count;
-Memory.rooms.E4N9.transportNeed = 1 - Memory.rooms.E4N9.transportCount;
-
-var creepsInRoom = getCreepsInRoom(E4N9);
-if(creepsInRoom != Memory.rooms.E4N9.needAllCreepsInRoom){
-	spawn.spawnCreeps(E4N9, Spawn1);
+var creepsInRoom = globals.getCreepsInRoom(room1);
+if(creepsInRoom.length < spawn.needAllCreepsInRoom){
+    var calcEnergy = spawn.calcEnergy(room1);
+	spawn.spawnCreeps(room1, spawn1, calcEnergy);
 }
 
-for (var i = 0; i < creepsInRoom.length; i++) {
+for(var i = 0; i < creepsInRoom.length; i++) {
 	var creep = creepsInRoom[i];
+	if(creep.memory.role == 'upgrader'){
+	    upgrade.upgrade(creep);
+	}
 	if(creep.memory.role == 'harvester'){
-		if(creep.carry.energy != creep.carryCapacity){
-			harvester.harvest(creep, E4N9);
-		}
-		else{
-			harvester.transferToLink(creep);
-		}
+		var room = globals.getRoomCreep(creep);
+		harvester.harvests(creep, room);
 	}
 	if(creep.memory.role == 'builder'){
 		var clean = creeps.suicideCreep(creep);
@@ -52,8 +35,11 @@ for (var i = 0; i < creepsInRoom.length; i++) {
 				creeps.takeEnergy(creep);
 			}
 			else{
-				builder.build(creep);
+				builder.builds(creep);
 			}
+		}
+		else{
+		    creeps.suicideCreep(creep);
 		}
 	}
 	if(creep.memory.role == 'scout'){
@@ -66,11 +52,17 @@ for (var i = 0; i < creepsInRoom.length; i++) {
 				scout.transferToStore(creep);
 			}
 		}
+		else{
+		    creeps.suicideCreep(creep);
+		}
 	}
 	if(creep.memory.role == 'mechanic'){
 		var clean = creeps.suicideCreep(creep);
 		if(clean == 0){
 			mechanic.repairRamparts(creep);
+		}
+		else{
+		    creeps.suicideCreep(creep);
 		}
 	}
 	if(creep.memory.role == 'mechanic1'){
@@ -78,18 +70,22 @@ for (var i = 0; i < creepsInRoom.length; i++) {
 		if(clean == 0){
 			mechanic1.repairRoad(creep);
 		}
-	}
-	if(creep.memory.role == 'build'){
-		var clean = creeps.suicideCreep(creep);
-		if(clean == 0){
-			builder.build(creep);
+		else{
+		    creeps.suicideCreep(creep);
 		}
 	}
 	if(creep.memory.role == 'transport'){
 		var clean = creeps.suicideCreep(creep);
 		if(clean == 0){
-			transport.transportTo(creep);
+		    if(creep.carry.energy == 0){
+				creeps.takeEnergy(creep);
+			}
+			else{
+			    transport.transportTo(creep);
+			}
+		}
+		else{
+		    creeps.suicideCreep(creep);
 		}
 	}
 }
-
